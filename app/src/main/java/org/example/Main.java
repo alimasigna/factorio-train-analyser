919b8d75a60e412d9d6013307987161ae2f1f48c
TheHKT <hworschinski@gmail.com>
2023-11-02 21:38:22 +0100

    feat: added array mapping of entities

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.jgrapht.Graph;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -18,8 +19,8 @@ public class Main {
         Json parsedInput;
         try{
             parsedInput = gson.fromJson(inputJson, Json.class);
-            Entity[] entities = parsedInput.getBlueprint().getEntities();
-
+            Entity[] entities = parsedInput.getBlueprint().getEntities(); //TODO only parse tracks, signals, etc
+            createBetterMatrix(entities);
             for (Entity entity : entities ) {
                 System.out.println("############");
                 System.out.println(entity.name);
@@ -29,6 +30,94 @@ public class Main {
         } catch (Exception e) {
             System.out.printf(e.getMessage());
         }
+    }
+
+    /*public Entity[][] createMatrix(Entity[] entities) {
+        //TODO only take entities that are tracks or signals todo this is old cz we have tracks on teh same x nad y
+        double lowX = entities[0].position.x;
+        double highX = entities[0].position.x;
+        double lowY = entities[0].position.y;
+        double highY = entities[0].position.y;
+        for(Entity entity : entities) {
+            if(entity.position.x < lowX){
+                lowX = entity.position.x;
+            }
+            if(entity.position.x > highX){
+                highX = entity.position.x;
+            }
+            if(entity.position.y < lowY) {
+                lowY = entity.position.y;
+            }
+            if(entity.position.y > highY) {
+                highY = entity.position.y;
+            }
+        }
+        int xDimensionLength = (int) Math.abs(highX - lowX) + 1;
+        int yDimensionLength = (int) Math.abs(highY - lowY) + 1;
+        Entity[][] entityMatrix = new Entity[xDimensionLength][yDimensionLength];
+        for(Entity entity : entities) {
+            int adjustedX = (int) (entity.position.x - lowX);
+            int adjustedY = (int) (entity.position.y - lowY);
+            entityMatrix[adjustedX][adjustedY] = entity;
+        }
+        for(int i = 0; i< entityMatrix.length; i++){
+            for(int j = 0; j<entityMatrix[i].length; j++){
+                if(entityMatrix[i][j] == null) {
+                    System.out.print(".");
+                    continue;
+                }
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+        return entityMatrix;
+    }*/
+
+    public ArrayList<Entity>[][] createBetterMatrix(Entity[] entities) {
+        double lowX = entities[0].position.x;
+        double highX = entities[0].position.x;
+        double lowY = entities[0].position.y;
+        double highY = entities[0].position.y;
+        for(Entity entity : entities) {
+            if(entity.position.x < lowX){
+                lowX = entity.position.x;
+            }
+            if(entity.position.x > highX){
+                highX = entity.position.x;
+            }
+            if(entity.position.y < lowY) {
+                lowY = entity.position.y;
+            }
+            if(entity.position.y > highY) {
+                highY = entity.position.y;
+            }
+        }
+        int xDimensionLength = (int) Math.abs(highX - lowX) + 1;
+        int yDimensionLength = (int) Math.abs(highY - lowY) + 1;
+        ArrayList<Entity>[][] entityMatrix = new ArrayList[xDimensionLength][yDimensionLength];
+        for(Entity entity : entities) {
+            int adjustedX = (int) (entity.position.x - lowX);
+            int adjustedY = (int) (entity.position.y - lowY);
+            if(entityMatrix[adjustedX][adjustedY] == null)
+                entityMatrix[adjustedX][adjustedY] = new ArrayList<Entity>();
+            entityMatrix[adjustedX][adjustedY].add(entity);
+        }
+
+        for(int i = 0; i< entityMatrix.length; i++){
+            for(int j = 0; j<entityMatrix[i].length; j++){
+                if(entityMatrix[i][j] == null) {
+                    System.out.print(".");
+                    continue;
+                }
+                if (entityMatrix[i][j].size()>1) {
+                    System.out.print("+");
+                    continue;
+                }
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+        return null;
     }
     public String readJsonFile() {
         System.out.println("Please enter Factorio JSON: ");
