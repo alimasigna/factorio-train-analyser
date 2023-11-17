62507fe575c325b0109e304be34af5b454d85195
TheHKT <hworschinski@gmail.com>
2023-11-17 18:07:09 +0100

    fix: better comment formatting

--- app/src/main/java/factorio/train/analyser/Matrix.java
+++ app/src/main/java/factorio/train/analyser/Matrix.java
@@ -11,24 +11,32 @@ import java.util.ArrayList;
 public class Matrix {
 
     /** This is the Json from which the Matrix will be generated from.
-     */
+     * */
     private Json json;
 
     /** This array will represent the given json in a matrix.
      * */
     private ArrayList<Entity>[][] matrix;
+
     /** Creates a speicific matrix object with a given jsonString.
      * @param jsonAsString The JsonString that will be used to generate a Json object.
      * */
     public Matrix(String jsonAsString){ //TODO change this to the class Laurin will create!
-        initJson(jsonAsString);
+        setJson(jsonAsString);
     }
 
     /** This will initialize the private Json field with a given Json String.
      * @param jsonAsString The JsonString that will be parsed into a Json object.
      * */
-    private void initJson(String jsonAsString) { //TODO change this to the class Laurn will create!
+    private void setJson(String jsonAsString) { //TODO change this to the class Laurn will create!
         JsonBuilder builder = new JsonBuilder();
         json = builder.create(jsonAsString);
+    }
+
+    /**This will initialize the private Matrix field from the Json field.
+     * */
+
+    private void setMatrix() {
+
     }
 }
--- app/src/main/java/factorio/train/analyser/jsonmodels/JsonBuilder.java
+++ app/src/main/java/factorio/train/analyser/jsonmodels/JsonBuilder.java
@@ -4,8 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /** This class is used to create a Json Object.
- */
+ * */
 public class JsonBuilder {
+
     /** Creates a JSON Object from a given JSON string.
      * @param jsonString The string that shall be converted to an object representation.
      * @return The generated Json Object if the parsing was successful. If the parsing didn't work it will return null.
