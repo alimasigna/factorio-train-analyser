72a75e6ac795014e3cdb6f07956292e2dfdf5c22
TheHKT <hworschinski@gmail.com>
2024-02-01 16:13:44 +0100

    cleanup

--- app/src/main/java/factorio/train/analyser/graph/Graph.java
+++ app/src/main/java/factorio/train/analyser/graph/Graph.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 public class Graph {
     private Section[] sections;
 
-    private ArrayList<Node> nodesFIELD;
+    private ArrayList<Node> nodes;
     private Matrix matrix;
 
     public Graph(String encodedString){
-        nodesFIELD = new ArrayList<>();
+        nodes = new ArrayList<>();
         setMatrix(encodedString);
         setNodes();
     }
@@ -21,52 +21,27 @@ public class Graph {
         if(encodedString!=null) matrix = new Matrix(encodedString);
     }
 
-    private void setNodes() { //TODO this method is a wip.
+    private void setNodes() {
         if(matrix == null) return;
 
-        ArrayList<Entity> knownEntries = new ArrayList<>();
-        ArrayList<Entity>[][] entries = matrix.getMatrix();
+        ArrayList<Track> knownTracks = new ArrayList<>();
         ArrayList<Track>[][] tracks = matrix.convertToTracks();
 
-        for( int x = 0; x < tracks.length; x++) {
-            for( int y = 0; y < entries[x].length; y++) {
-                if(entries[x][y] == null) continue;
-                for( int i = 0; i < entries[x][y].size(); i++) {
-                    Entity entry = entries[x][y].get(i);
-                    if(knownEntries.contains(entry) || !( entry.getName().equals("straight-rail") || entry.getName().equals("curved-rail") )) continue; //we only use rails skip everything
+        for( int x = 0; x < tracks.length; x++) {          //go through all tracks, we only go through tracks. signals should be discovered this way too
+            for (int y = 0; y < tracks[x].length; y++) {
+                if (tracks[x][y] == null) continue;
+                for (int i = 0; i < tracks[x][y].size(); i++) {
                     Track track = tracks[x][y].get(i);
-                    recLeftNode(track, tracks, entries, knownEntries);
+                    if (knownTracks.contains(track)) continue; //we only use tracks skip everything
+                    recLeftNode(track, tracks, knownTracks);
                 }
             }
         }
-
-        /*ArrayList<Node> Nodes = new ArrayList<>();
-        for( int x = 0; x < entries.length; x++) {
-            for( int y = 0; y < entries[x].length; y++) {
-                if(entries[x][y] == null) continue;
-                for( int i = 0; i < entries[x][y].size(); i++) {
-                    Entity entry = entries[x][y].get(i);
-                    if(knownEntries.contains(entry) || !( entry.getName().equals("straight-rail") || entry.getName().equals("curved-rail") )) continue; //we only use rails skip everything
-                    recTest(Nodes, entry, null, knownEntries, entries);
-                }
-            }
-        }*/
+        System.out.println("XDD");
     }
 
-    private void recRightNode(Track currentTrack, ArrayList<Track>[][] tracks, ArrayList<Entity>[][] entries, ArrayList<Entity> knownEntries) {
-        Track[] right = filterLookupsToTrack(currentTrack.getConnected()[1], tracks);
-        if(currentTrack.getNodes() == null) {
-            recLeftNode(currentTrack, tracks, entries, knownEntries);
-        }
-        if(right.length == 0) { //if we got all left nodes, we go for the right ones
-            return;
-        } else {
-            for(int i = 0; i<right.length; i++) {
-                recLeftNode(right[i], tracks, entries, knownEntries);
-            }
-        }
-    }
-    private void recLeftNode(Track currentTrack, ArrayList<Track>[][] tracks, ArrayList<Entity>[][] entries, ArrayList<Entity> knownEntries) {
+    private void recLeftNode(Track currentTrack, ArrayList<Track>[][] tracks, ArrayList<Track> knownTracks) {
+        knownTracks.add(currentTrack);
         Track[] left = filterLookupsToTrack(currentTrack.getConnected()[0], tracks);
         Track[] right = filterLookupsToTrack(currentTrack.getConnected()[1], tracks);
 
@@ -80,7 +55,7 @@ public class Graph {
             for(Track trackLeft : left) { //go thorugh all left tracks
                 ArrayList<Node> parentNodesLeft = trackLeft.getNodes();
                 if(parentNodesLeft.size() == 0) { //if we have no parents recursivly call this method
-                    recLeftNode(trackLeft, tracks, entries, knownEntries);
+                    recLeftNode(trackLeft, tracks, knownTracks);
                 } //we go down to basecas  there might be loops after we reach basecase we should have a parent
                 // allLeftParentNodes.addAll(parentNodesLeft); //we add all found nodes on the left
             }
@@ -111,16 +86,16 @@ public class Graph {
 
         //now we add the currentParentNodes to the overall Nodespool
         for(Node parent : currentParentNodes) {
-            if(!nodesFIELD.contains(parent)) {
-                nodesFIELD.add(parent);
+            if(!nodes.contains(parent)) {
+                nodes.add(parent);
             }
         }
 
         if(right.length == 0) { //if we got all left nodes, we go for the right ones
             return;
         } else {
-            for(int i = 0; i<right.length; i++) { //TODO endless rec
-                recLeftNode(right[i], tracks, entries, knownEntries);
+            for(int i = 0; i<right.length; i++) {
+                recLeftNode(right[i], tracks, knownTracks);
             }
         }
 
@@ -134,39 +109,6 @@ public class Graph {
             track.addParentNode(node);
         }
     }
-    /*private void recTest(ArrayList<Node> Nodes, Entity entry, Node currentNode, ArrayList<Entity> knownEntries, ArrayList<Entity>[][] entries){
-        if(knownEntries.contains(entry)) return; //if we already visited this track we can skip it
-        knownEntries.add(entry);
-        Track track;
-        if(currentNode == null) {   // we currently have no node selected
-            currentNode = new Node();
-        }
-        track = new Track(currentNode, entry.getName().equals("straight-rail") ? 1 : 3, entry.getDirection(), (int) entry.getPosition().getX(), (int) entry.getPosition().getY());
-        currentNode.addTrack(track);
-        if(!Nodes.contains(currentNode)) Nodes.add(currentNode);
-
-        Entity[] connectionsL = filterLookups(entry.getConnected()[0], entries);
-        Entity[] connectionsR = filterLookups(entry.getConnected()[1], entries);
-
-        Node clone = currentNode.cloneNode(); // we always clone the current node incase there might be multiple new connections
-        int counter = 0;
-        for(Entity connection : connectionsL) {
-            if(knownEntries.contains(connection)) {
-                //TODO add nodes to the current track, if we are on an end it should stop
-                continue;
-            }
-            if(counter > 0) { //if we have more connection we gotta make new nodes
-                recTest(Nodes, connection, clone, knownEntries, entries);
-            } else {
-                recTest(Nodes, connection, currentNode, knownEntries, entries);
-            }
-
-            counter++;
-        }
-        System.out.println("WE LEFT REC");
-        Entity[] crossings = filterLookups(entry.getCrossed(), entries);
-
-    }*/
 
     //removes all LookUps to empty or non-existent coordinates
     private static Entity[] filterLookups(LookUp[] lookUps, ArrayList<Entity>[][] entries) {
