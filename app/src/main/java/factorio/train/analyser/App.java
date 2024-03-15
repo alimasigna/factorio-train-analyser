f5437f69af23f1592ae8fcb0f0e4103f63e26015
TheHKT <hworschinski@gmail.com>
2024-03-15 11:28:49 +0100

    reafactored App.java, debuggin is now possible again!

--- app/src/main/java/factorio/train/analyser/App.java
+++ app/src/main/java/factorio/train/analyser/App.java
@@ -11,36 +11,61 @@ import com.google.gson.JsonIOException;
 
 import factorio.train.analyser.analyser.DeadlockAnalyser;
 import factorio.train.analyser.analyser.Result;
-import factorio.train.analyser.decoder.Decoder;
 import factorio.train.analyser.graph.Graph;
 import factorio.train.analyser.jsonmodels.Output;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
+    public String[] args;
 
-    private static final String blueprint = "0eNq9mttu4kAMht9lrgPKnGd4lVW14hDRSDSgANVWFe++CbSc6iy/jbQ3lQLJV+cf22N7+FSz1b7atHWzU5NPVc/XzVZNfn2qbb1spqv+s93HplITVe+qN1WoZvrWX7XTeqUOhaqbRfVHTfThpVBVs6t3dXV6/njx8bvZv82qtrvh/OR21z27fN2NjohCbdbb7ql10/+rjuR8LtSHmoyMix1/UbfV/PS1PhQ/sAbGBj2A9QTWnrHzffteLQah7hvqbqGRgLob8UZfAlOWjv3Z1rG/BVsC7AUi+MfaBlCENCSCIaARtjXFAVspbdOttvPXad0MK5z8ReEwBqTIAqvj44XTJR4VZ4PtY3P1Jdpm9XJUrbqb23o+2qxXFcVO10p3GaA5wbf9Dbr/01aL66Cuu6ts+5BftlXVUF8dOlJTdW80W+/bPiNk+0JZyghgO+APVABrK+AiyjqWo4V4cTR372hUeGg8ltOQR1DxoYOAC6QeHVmelvJ1qvi/npbwWIsDypKelgVcDexseG4InmGv0QIuYi8jkvOAvVREGDySo+ZwHc61HC4ewdFzuHgEx8jh4jtyZK0bHm/JDvgZlclMFnDt40xm8XjLmmGv1QIuYq9h7UT+quTRY6CislZQsWugtHZ8rs2P84/1aM1uvqkJUAGPuhA5KkRxHaGRlsAmvt2Qyozo0wNcsjcq+dx7nSkdnEabmDDgFVRuc/helzJHBcvnQiqgrWe+aj3LMVBnO0Hvea8EyQ0CLjAvcJFfpUD2JgEXsTfzd+d7LuVpvhRwMzCK0Ky8FtPZ32xGGnFv+J04pIcVcBE9HL8PtcCAynsBF9FXsN/d20tlTB+fjw+Sm/jdAaRvfp5L2RtKAReZ1Wl+NwPZa/jdDMS1/G7GAlVrcAIuYq9/nkvlhxCez++kvVFQpyHcxJ8jQeuWBVxA3yiYolig7omCKYoF5ouR19WFq/3T3++fgeJbQZ5A9HCCPAHMLaMX2IvoHHh1iv2HzqTfRYEeCJd30BEd1z8E0xVkHVMp4AJ6JF69mQxTj8SLxxT+wafiJllBvQVMeZMTcIEpb0KnLPE8ZSkBKnrUGMOFSnEkux4ww0tJwAVmeCmj711+UU2i3juXKCddOI8PPbV0RmUyMmHMhl8LG0++v2WdQsXvA2vDOIXS1xIdP4iDx1LkvT/OqbQudKSOqrKgiB0QxqO/OciXxev2OpIVBMtlSVIUvB9NStJxw/Etb1zU9ct5/JHL5Oo3MYV6r9rtqR5OuivCTUwu25DD4fAX/AZqTA==";
+    // use these field for debugging purposes only!
+    private final String debugBlueprint = "0eNq9mttu4kAMht9lrgPKnGd4lVW14hDRSDSgANVWFe++CbSc6iy/jbQ3lQLJV+cf22N7+FSz1b7atHWzU5NPVc/XzVZNfn2qbb1spqv+s93HplITVe+qN1WoZvrWX7XTeqUOhaqbRfVHTfThpVBVs6t3dXV6/njx8bvZv82qtrvh/OR21z27fN2NjohCbdbb7ql10/+rjuR8LtSHmoyMix1/UbfV/PS1PhQ/sAbGBj2A9QTWnrHzffteLQah7hvqbqGRgLob8UZfAlOWjv3Z1rG/BVsC7AUi+MfaBlCENCSCIaARtjXFAVspbdOttvPXad0MK5z8ReEwBqTIAqvj44XTJR4VZ4PtY3P1Jdpm9XJUrbqb23o+2qxXFcVO10p3GaA5wbf9Dbr/01aL66Cuu6ts+5BftlXVUF8dOlJTdW80W+/bPiNk+0JZyghgO+APVABrK+AiyjqWo4V4cTR372hUeGg8ltOQR1DxoYOAC6QeHVmelvJ1qvi/npbwWIsDypKelgVcDexseG4InmGv0QIuYi8jkvOAvVREGDySo+ZwHc61HC4ewdFzuHgEx8jh4jtyZK0bHm/JDvgZlclMFnDt40xm8XjLmmGv1QIuYq9h7UT+quTRY6CislZQsWugtHZ8rs2P84/1aM1uvqkJUAGPuhA5KkRxHaGRlsAmvt2Qyozo0wNcsjcq+dx7nSkdnEabmDDgFVRuc/helzJHBcvnQiqgrWe+aj3LMVBnO0Hvea8EyQ0CLjAvcJFfpUD2JgEXsTfzd+d7LuVpvhRwMzCK0Ky8FtPZ32xGGnFv+J04pIcVcBE9HL8PtcCAynsBF9FXsN/d20tlTB+fjw+Sm/jdAaRvfp5L2RtKAReZ1Wl+NwPZa/jdDMS1/G7GAlVrcAIuYq9/nkvlhxCez++kvVFQpyHcxJ8jQeuWBVxA3yiYolig7omCKYoF5ouR19WFq/3T3++fgeJbQZ5A9HCCPAHMLaMX2IvoHHh1iv2HzqTfRYEeCJd30BEd1z8E0xVkHVMp4AJ6JF69mQxTj8SLxxT+wafiJllBvQVMeZMTcIEpb0KnLPE8ZSkBKnrUGMOFSnEkux4ww0tJwAVmeCmj711+UU2i3juXKCddOI8PPbV0RmUyMmHMhl8LG0++v2WdQsXvA2vDOIXS1xIdP4iDx1LkvT/OqbQudKSOqrKgiB0QxqO/OciXxev2OpIVBMtlSVIUvB9NStJxw/Etb1zU9ct5/JHL5Oo3MYV6r9rtqR5OuivCTUwu25DD4fAX/AZqTA==";
+    private boolean isInDebug = false;
 
-    public static boolean trainAnalyser(String blueprint) throws JsonIOException, IOException {
-        Matrix matrix = new Matrix(blueprint);
+    public App(String[] args) {
+        if (args != null) {
+            this.args = args;
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App(args);
+        app.run();
+    }
+
+    public void run() {
+        try {
+            if (args.length > 0) {
+                printResult(analyse(readFromFile()));
+            } else if (isInDebug) {
+                System.out.println("Debug mode is active. Using the given String!");
+                printResult(analyse(this.debugBlueprint));
+            } else {
+                System.out.println("No argmuents were given.");
+            }
+        } catch (JsonIOException e) {
+            System.out.println("Error while exporting to json.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error while reading the file.");
+            e.printStackTrace();  
+        }
+    }
+
+    public boolean analyse(String blueprint) throws JsonIOException, IOException{
+        if (blueprint == null) {
+            System.out.println("No blueprint found.");
+        }
         Graph graph = new Graph(blueprint);
-        graph.getClass();
-        Decoder decoder = new Decoder();
-        String decodedString = decoder.decode(blueprint);
-        DeadlockAnalyser analyser = new DeadlockAnalyser();
-        analyser.setGraph(graph);
+        DeadlockAnalyser analyser = new DeadlockAnalyser(graph);
         ArrayList<Result> result = analyser.deadlockCheck();
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-        Output output = new Output(result, graph);
-        FileWriter writer = new FileWriter("./output.json");
-        gson.toJson(output, writer);
-        writer.close();
+        exportToJson(result, graph);
 
         for (int i = 0; i < result.size(); i++) {
             if (result.get(i).getIsDeadlock()) {
@@ -50,33 +75,35 @@ public class App {
         return false;
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public void exportToJson(ArrayList<Result> result, Graph graph) throws JsonIOException, IOException {
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+            Output output = new Output(result, graph);
+            FileWriter writer = new FileWriter("./output.json");
+            gson.toJson(output, writer);
+            writer.close();
+    }
 
-        if (args.length > 0) {
-            String url = args[0];
-            File blueprintFile = new File(url);
-            try (BufferedReader br = new BufferedReader(new FileReader(blueprintFile))) {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
+    private String readFromFile() throws FileNotFoundException, IOException{
+        String url = args[0];
+        File blueprintFile = new File(url);
+        try (BufferedReader br = new BufferedReader(new FileReader(blueprintFile))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-                while (line != null) {
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
-                    line = br.readLine();
-                }
-                String finalBlueprint = sb.toString();
-                boolean result = trainAnalyser(finalBlueprint.trim());
-                
-                /*System.out.println(
-                        "\n \n ------------------------------------------------------------------------------\n \n");
-                System.out.println(result ? "Deadlock" : "No Deadlock");
-                System.out.println(
-                        "\n \n ------------------------------------------------------------------------------");*/
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
             }
-
-        } else {
-            System.out.println("Kein Inhalt angegeben.");
+            return sb.toString();
         }
+    }
 
+    private void printResult(boolean deadlockFound) {
+        if (deadlockFound) {
+            System.out.println("Deadlock found! More information in output.json");
+        } else {
+            System.out.println("No deadlock found! More information in output.json");
+        }
     }
 }
